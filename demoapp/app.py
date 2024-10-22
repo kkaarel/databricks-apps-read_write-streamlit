ac2a5838fd297d31024484427f09e681b91ae603
kkaarel <kaarel.korvemaa@kkaarel.com>
2024-10-22 13:54:34 +0300

    copypaste

--- demoapp/app.py
+++ demoapp/app.py
@@ -1,59 +1,29 @@
-import os
 from databricks import sql
-from databricks.sdk.core import Config, oauth_service_principal
+from databricks.sdk.core import Config
 import streamlit as st
 import pandas as pd
-import requests
 
 # Ensure environment variable is set correctly
+assert os.getenv('DATABRICKS_WAREHOUSE_ID'), "DATABRICKS_WAREHOUSE_ID must be set in app.yaml."
+
+def sqlQuery(query: str) -> pd.DataFrame:
+    cfg = Config() # Pull environment variables for auth
+    with sql.connect(
+        server_hostname=cfg.host,
+        http_path=f"/sql/1.0/warehouses/{os.getenv('DATABRICKS_WAREHOUSE_ID')}",
+        credentials_provider=lambda: cfg.authenticate
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            return cursor.fetchall_arrow().to_pandas()
+
 st.set_page_config(layout="wide")
 
-
-
-#assert os.getenv('DATABRICKS_WAREHOUSE_ID'), "DATABRICKS_WAREHOUSE_ID must be set in app.yaml."
-
-# Ensure that secrets are correctly set up
-#try:
-#    DATABRICKS_WAREHOUSE_ID = st.secrets["DATABRICKS_WAREHOUSE_ID"]
-#    st.write("DATABRICKS_WAREHOUSE_ID:", DATABRICKS_WAREHOUSE_ID)
-#except FileNotFoundError:
-#    st.error("No secrets found. Please make sure that the secrets.toml file is properly configured.")
-#    st.stop()
-
-headers = st.context.headers
-with st.expander("Headers"):
-    st.write(headers)
-with st.expander("Environment variables"):
-    st.write(dict(os.environ))
-
-app_name = os.getenv("DATABRICKS_APP_NAME", "Not Found")
-client_id = os.getenv("DATABRICKS_CLIENT_ID", "Not Found")
-host = os.getenv("DATABRICKS_HOST", "Not Found")
-warehouse_id = os.getenv("DATABRICKS_WAREHOUSE_ID", "Not Found")
-
-
-# Display the extracted environment variables
-st.write("App Name:", app_name)
-st.write("Client ID:", client_id)
-st.write("Host:", host)
-st.write("Warehouse ID:", warehouse_id)
-
-
-
-
-def credential_provider():
-  config = Config(
-    host          = f"https://{host}",
-    client_id     = os.getenv("DATABRICKS_CLIENT_ID"),
-    client_secret = os.getenv("DATABRICKS_CLIENT_SECRET"))
-  return oauth_service_principal(config)
-
-with sql.connect(server_hostname      = host,
-                 http_path            = f"/sql/1.0/warehouses/{warehouse_id}",
-                 credentials_provider = credential_provider) as connection:
-    st.write("Successfully connected to Databricks SQL Warehouse")
-
-@st.cache_data(ttl=600)  # only re-query if it's been 600 seconds
+@st.cache_data(ttl=30)  # only re-query if it's been 30 seconds
 def getData():
-    # Update query to use the new people table
-    return sqlQuery("select * from app_dev.default.people limit 5000")
+    # This example query depends on the nyctaxi data set in Unity Catalog, see https://docs.databricks.com/en/discover/databricks-datasets.html for details
+    return sqlQuery("select * from app_dev.default.people ")
+
+data = getData()
+
+st.write(data)
