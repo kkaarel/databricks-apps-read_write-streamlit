e6e50c5db563f806c5248bfd294f942819df512d
kkaarel <kaarel.korvemaa@kkaarel.com>
2024-10-22 12:58:22 +0300

    network problem

--- demoapp/app.py
+++ demoapp/app.py
@@ -22,7 +22,7 @@ except FileNotFoundError:
 
 def sqlQuery(query: str) -> pd.DataFrame:
     cfg = Config() # Pull environment variables for auth
-
+    DATABRICKS_WAREHOUSE_ID = st.secrets["DATABRICKS_WAREHOUSE_ID"]
     with sql.connect(
         server_hostname=cfg.host,
         http_path=f"/sql/1.0/warehouses/{DATABRICKS_WAREHOUSE_ID}",
