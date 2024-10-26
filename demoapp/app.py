d31ad663c73dd725b1524e21a175f38bdb44b3fa
kkaarel <kaarel.korvemaa@kkaarel.com>
2024-10-26 12:10:00 +0300

    update

--- demoapp/app.py
+++ demoapp/app.py
@@ -14,8 +14,9 @@ st.set_page_config(
 # Ensure environment variable is set correctly
 #assert os.getenv('DATABRICKS_WAREHOUSE_ID'), "DATABRICKS_WAREHOUSE_ID must be set in app.yaml."
 
-#DATABRICKS_WAREHOUSE_ID = "38c9b53fe30ba3a0"  #st.write(os.getenv('DATABRICKS_WAREHOUSE_ID'))
+#DATABRICKS_WAREHOUSE_ID = ""  #st.write(os.getenv('DATABRICKS_WAREHOUSE_ID'))
 
+st.write(os.environ)
 
 DATABRICKS_WAREHOUSE_ID = st.text_input(
     "Enter Databricks Warehouse ID:",
@@ -27,7 +28,7 @@ if not DATABRICKS_WAREHOUSE_ID:
     st.error("DATABRICKS_WAREHOUSE_ID must be provided.")
     st.stop()
 
-#st.write(os.environ)
+
 
 def get_user_info():
     headers = st.context.headers
@@ -60,6 +61,12 @@ def sqlQuery(query: str) -> pd.DataFrame:
         return pd.DataFrame()  # Return an empty DataFrame in case of error
 
 
+if 'stage' not in st.session_state:
+    st.session_state.stage = 0
+
+def set_state(i):
+    st.session_state.stage = i
+
 
 #@st.cache_data(ttl=600)  # IF you want to query realtime and see the changes using this is not smart
 def getData():
@@ -89,70 +96,77 @@ else:
     update_button = st.button('Update Rows')
 
     if update_button:
-        st.write("Updating rows...")
-        cfg = Config()
-        with sql.connect(
-            server_hostname=cfg.host,
-            http_path=f"/sql/1.0/warehouses/{DATABRICKS_WAREHOUSE_ID}",
-            credentials_provider=lambda: cfg.authenticate
-        ) as connection:
-            with connection.cursor() as cursor:
-                for index, row in filtered_df.iterrows():
-                    try:
-                        # Assuming 'id' is the primary key and should not be updated
-                        columns = [col for col in row.index if col != "id" and col != "Select"]
-                        updates = ", ".join([f"{col} = '{row[col]}'" for col in columns])
-                        update_query = f"UPDATE app_dev.default.people SET {updates} WHERE id = {row['id']}"
-                        cursor.execute(update_query)
-                    except Exception as e:
-                        st.error(f"An error occurred while updating row {index}: {e}")
-                        st.stop()
+        with st.spinner('Updating rows...'):
+            cfg = Config()
+            with sql.connect(
+                server_hostname=cfg.host,
+                http_path=f"/sql/1.0/warehouses/{DATABRICKS_WAREHOUSE_ID}",
+                credentials_provider=lambda: cfg.authenticate
+            ) as connection:
+                with connection.cursor() as cursor:
+                    for index, row in filtered_df.iterrows():
+                        try:
+                            # Assuming 'id' is the primary key and should not be updated
+                            columns = [col for col in row.index if col != "id" and col != "Select"]
+                            updates = ", ".join([f"{col} = '{row[col]}'" for col in columns])
+                            update_query = f"UPDATE app_dev.default.people SET {updates} WHERE id = {row['id']}"
+                            cursor.execute(update_query)
+                        except Exception as e:
+                            st.error(f"An error occurred while updating row {index}: {e}")
+                            st.stop()
 
-        data = getData()
-        st.write("Updated Data:")
-        st.write(data)
+            data = getData()
+            st.write("Updated Data:")
+            st.write(data)
         
+#if st.session_state.stage == 0:
+#    st.button('Begin', on_click=set_state, args=[1])
 
-
-
+set_state(1)
 with st.expander("Add rows to data"):
-    with st.form(key='user_form'):
+    if st.session_state.stage >= 1:
+        with st.form(key='user_form'):
 
-        inputs = {}
-        for column in data.columns:
-    
-            inputs[column] = st.text_input(label=column)
+            inputs = {}
+            for column in data.columns:
+
+                if column != "Select":
+                    inputs[column] = st.text_input(label=column)
 
 
-        submit_button = st.form_submit_button(label='Submit')
+            submit_button = st.form_submit_button(label='Submit', on_click=set_state, args=[2])
 
 
-    if submit_button:
+        if submit_button:
 
-        user_input_data = pd.DataFrame([inputs])
-        st.write('Submitted Data:')
-        st.write(user_input_data)
-            
-        cfg = Config() 
-        with sql.connect(
-            server_hostname=cfg.host,
-            http_path=f"/sql/1.0/warehouses/{DATABRICKS_WAREHOUSE_ID}",
-            credentials_provider=lambda: cfg.authenticate
-        ) as connection:
-            with connection.cursor() as cursor:
-                
-                try:
-           
-                    table_name = "app_dev.default.people"
+            user_input_data = pd.DataFrame([inputs])
+            st.write('Submitted Data:')
+            with st.spinner('Updating rows...'):
+            #st.write(user_input_data)
+                if st.session_state.stage >= 2:
+                    set_state(3)
+                        
+                    cfg = Config() 
+                    with sql.connect(
+                        server_hostname=cfg.host,
+                        http_path=f"/sql/1.0/warehouses/{DATABRICKS_WAREHOUSE_ID}",
+                        credentials_provider=lambda: cfg.authenticate
+                    ) as connection:
+                        with connection.cursor() as cursor:
+                            
+                            try:
+                    
+                                table_name = "app_dev.default.people"
 
-                    cursor.execute(f"INSERT INTO {table_name} VALUES {tuple(user_input_data.iloc[0])}")
+                                cursor.execute(f"INSERT INTO {table_name} VALUES {tuple(user_input_data.iloc[0])}")
 
-                    data = getData()
-                    st.write(f"Data from {table_name}")
-                    st.write(data)
-                except Exception as e:
-                    st.error(f"An error occurred while inserting data: {e}")
-                    st.stop()
+                                data = getData()
+                                st.write(f"Data from {table_name}")
+                                st.write(data)
+
+                            except Exception as e:
+                                st.error(f"An error occurred while inserting data: {e}")
+                                st.stop()
 
 
 
